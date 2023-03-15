340d813a1c7fd0657808cde8256c56b4fc920e3f
Mohaimin Islam <92102672+CodeWithMohaimin@users.noreply.github.com>
2023-03-15 12:02:02 +0600

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,25 @@
-# misti.com
+# Just Play ▶ with HTM, CSS and Javascript 
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Features
+
+- A responsive design that works well on different devices.
+- A custom CSS grid layout.
+- Fast and easy to use
+
+## Links
+
+- [Live Demo](https://codewithmohaimin.github.io/misti.com/)
+- [Frontend Mentor Challenge](https://www.youtube.com/@CodeWithMohaimin)
+
+## Credits
+
+- Design by [Frontend Mentor](https://www.youtube.com/@CodeWithMohaimin).
+- QR code generation library: [QRCode.js](https://www.youtube.com/@CodeWithMohaimin)
+- Icons from [FontAwesome](https://www.youtube.com/@CodeWithMohaimin)
+
